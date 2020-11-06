279adc9f57334902f98a384bcb6a6f25005999c2
ifarzeen <farzeensaleem288@gmail.com>
2020-11-06 16:42:16 +0500

    Milestone-1

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="container">
-    <div class="filter-box">
-      <input type="text" placeholder="Filter..."/>
-      <button class="btn1"><img src=".././assets/icons8-search.svg" /></button>
-    </div>
-
     <table class="myDataTable">
-        <thead>
-          <tr>
-            <th>Sr#</th>
-            <th>Author</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        
-      <tr v-bind:key="index" v-for="(mydata, index) in tableData">
-        <td> {{index}}</td>
-        <td> {{mydata.data.author}}</td>
-        <td> {{mydata.data.title}}</td>
-      </tr>
-      
+      <thead>
+        <tr>
+          <th>Sr#</th>
+          <th>Author</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="index" v-for="(mydata, index) in tableData">
+          <td>{{ index + 1 }}</td>
+          <td>{{ mydata.data.author }}</td>
+          <td>{{ mydata.data.title }}</td>
+        </tr>
+      </tbody>
     </table>
-
-    
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import $ from 'jquery';
-// import dt from 'datatables.net'
+import $ from "jquery";
+//import dt from 'datatables.net'
 
 // var dt      = require( 'datatables.net' )();
 export default {
@@ -37,85 +30,55 @@ export default {
   data() {
     return {
       tableData: [],
-      specs: [
-        {
-          fName: "Farzeen",
-          lName: "Saleem"
-        },
-        {
-          fName: "Jordan",
-          lName: "West"
-        },
-      ],
     };
   },
-  async created(){
-  // var _this=this;
-  // var data;
-  //    axios.get("https://www.reddit.com/r/technology/new.json")
-  //    .then( (res) => { data =  res.data.data.children
-  //    console.log(res);
-  //       $(document).ready(function(){
-  //         console.log(data)
-  //         this.tableData=data;
-  //         console.log(this.tableData);
-  //      $('.myDataTable').DataTable();
-  //      console.log(this.tableData);
-  //   });
-  //   });
-    //   this.tableData=  data.data.data.children;
-    //   console.log(data);
-    //     $(document).ready(function(){
-    //    $('.myDataTable').DataTable();
-    // });
 
+  created() {},
+  async mounted() {
+    const res = await axios.get("https://www.reddit.com/r/technology/new.json");
+    this.tableData = res.data.data.children;
+    var table;
+    $(document).ready(function() {
+      table = $(".myDataTable")
+        .dataTable()
+        .api({
+          data: this.tableData,
+          columns: [{ title: "sr#" }, { title: "Author" }, { title: "Title" }],
+        });
+
+      const iets = $("input").attr("aria-name", "DataTables_Table_0");
+
+      //  console.log( iets[0].className);
+      console.log(iets[0]);
+
+      iets.unbind().bind("keyup", function() {
+        console.log(iets.val().length);
+        var myvalue = iets.val();
+        if (iets.val().length >= 3) {
+          console.log("heyy");
+          console.log(myvalue);
+          table.search(this.value).draw();
+        }
+        if (iets.val() === "" || iets.val().length == 0) {
+          console.log("Yo");
+          table.search(this.value).draw();
+        }
+      });
+    });
+
+    // if(iets){
+    //   iets.addEventListener('keyup',()=>{
+    //     console.log('called')
+    //      if(iets.val().length >= 3) {
+    //           dt.search(iets.val()).draw();
+    //       }
+    //       if(iets.val() === ""){
+    //           dt.search(iets.val()).draw();
+    //       }
+    //    });
+    // }
   },
- async mounted(){
-     axios.get("https://www.reddit.com/r/technology/new.json")
-     .then( (res) => { this.tableData =  res.data.data.children;
-     console.log(res);
-        console.log(this.tableData);
-        $(document).ready(function(){
-       
-  
-       $('.myDataTable').DataTable();
-   
-    });
-    });
-
-    //  axios
-    //   .get("https://www.reddit.com/r/technology/new.json")
-    //    .then((response) => (this.tableData = response.data.data.children));
-
-    //  $(document).ready(function(){
-    //    $('.myDataTable').DataTable();
-    // });
-    // $(document).ready(function() {
-    // $('.myDataTable').DataTable( {
-       
-    // } );})
-
-  }
-  // methods:{
-  //   initDatatable() {
-  //               setTimeout(() => {
-  //               $('.walla').DataTable({
-  //                   "pagingType": "full_numbers",
-  //                   "lengthMenu": [
-  //                       [10, 25, 50, -1],
-  //                       [10, 25, 50, "All"]
-  //                   ],
-  //                   order: [[0, 'asc'], [3, 'desc']],
-  //                   responsive: true,
-  //                   destroy: true,
-  //                   retrieve: true,
-  //                   autoFill: true,
-  //                   colReorder: true,
-
-  //               });
-  //              }, 300)
-  //           }
-  // }
+  updated() {},
 };
 </script>
 <style>
